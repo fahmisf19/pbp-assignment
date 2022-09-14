82a300b1facd9c846e929f6b663bbadb6a9e1762
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-09-14 18:42:02 +0700

    feat: add unit testing for show_katalog

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from katalog.views import show_katalog
 
 # Create your tests here.
+class tests(SimpleTestCase):
+
+    def test_show_katalog_url_resolves(self):
+        url = reverse('katalog:show_katalog')
+        self.assertEquals(resolve(url).func, show_katalog)
