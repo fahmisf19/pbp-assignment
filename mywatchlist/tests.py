2d9aa700798c6ad01e8a5507db1a7aec8165818a
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-09-19 22:40:28 +0700

    feat: add unit test to url

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,26 +1,18 @@
 from urllib import response
-from django.test import SimpleTestCase, Client, TestCase
-from django.urls import reverse, resolve
-from mywatchlist.views import show_mywatchlist, show_mywatchlist_json, show_mywatchlist_xml, show_mywatchlist_data
+from django.test import Client, TestCase
 
 # Create your tests here.
-class tests(SimpleTestCase):
+class tests(TestCase):
 
-    def setUp(self):
-        self.client = Client()
+    def test_url(self):
+        response = self.client.get('/mywatchlist/', follow=True)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_url_xml(self):
+        response = self.client.get('/mywatchlist/xml/', follow=True)
+        self.assertEqual(response.status_code, 200)
 
-    def test_show_mywatchlist_url_resolves(self):
-        url = reverse('mywatchlist:show_mywatchlist')
-        self.assertEquals(resolve(url).func, show_mywatchlist)
-
-    def test_show_mywatchlist_json_url_resolves(self):
-        url = reverse('mywatchlist:show_mywatchlist_json')
-        self.assertEquals(resolve(url).func, show_mywatchlist_json)
-
-    def test_show_mywatchlist_xml_url_resolves(self):
-        url = reverse('mywatchlist:show_mywatchlist_xml')
-        self.assertEquals(resolve(url).func, show_mywatchlist_xml)
-
-    # def test_test(self):
-    #     response = self.client.get('/show_mywatchlist/xml/', follow=True)
-    #     self.assertEquals(response.status_code, 200)
+    def test_url_json(self):
+        response = self.client.get('/mywatchlist/json/', follow=True)
+        self.assertEqual(response.status_code, 200)
+    
