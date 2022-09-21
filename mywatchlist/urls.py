f88db394d228061c2297b503f2ed729bc8bc73cc
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-09-21 10:14:59 +0700

    feat: add button to show report

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -5,7 +5,7 @@ from mywatchlist.views import show_mywatchlist, show_mywatchlist_xml, show_mywat
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
     path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
     path('json/<int:id>', show_mywatchlist_data, name='show_mywatchlist_data')
